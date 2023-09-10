d30019b9fe4b4a1d6b732cb89a3421c423ced8f0
Vologdin Semen <vologdin.dev@gmail.com>
2023-09-10 08:36:50 +0500

    create models

--- api/models/answers.go
+++ api/models/answers.go
@@ -1,1 +1,13 @@
 package models
+
+type Answer struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	CreatedDate string `json:"date_create"`
+	UpdatedDate string `json:"date_update"`
+	TaskId      string `json:"task_id"`
+}
+
+func (a Answer) GetTableName() string {
+	return "answers"
+}
--- api/models/task.go
+++ api/models/task.go
@@ -1,1 +1,17 @@
 package models
+
+import "github.com/jackc/pgx/pgtype"
+
+type Task struct {
+	Id          int         `json:"id"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	CreatedDate pgtype.Date `json:"date_create"`
+	UpdatedDate pgtype.Date `json:"date_update"`
+
+	Answers []Answer `json:"answers"`
+}
+
+func (t Task) GetTableName() string {
+	return "tasks"
+}
